91a2350c53f1b83e5845630acf0fff52ef051bc6
Chansong Jo <chansong.jo@navercorp.com>
2021-12-10 13:26:21 +0900

    done 10998

---
+++ bronze_10998/src/main.rs
@@ -0,0 +1,22 @@
+use std;
+
+fn read_from_stdin() -> String {
+    let mut line = String::new();
+    let stdin = std::io::stdin();
+    let res = stdin.read_line(&mut line);
+    match res {
+        Ok(_) => return line,
+        Err(_) => return String::from(r#"error occured"#)
+    }
+}
+
+fn main() {
+    let line = read_from_stdin();
+    let result = line.trim()
+        .split_whitespace()
+        .map(|num| num.parse::<f64>().unwrap())
+        .reduce(|a, b| a * b)
+        .unwrap();
+    
+    println!("{}", result);
+}
